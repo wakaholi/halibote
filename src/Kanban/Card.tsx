4c56dd8b24bf80b2daab07c1d2ff28abea05929a
wakaholi <tao6128@gmail.com>
2021-04-16 00:48:47 +0900

    CardにDraggableをよせてシンプルにした

--- src/Kanban/Card.tsx
+++ src/Kanban/Card.tsx
@@ -1,20 +1,43 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Draggable } from 'react-beautiful-dnd';
+import { CardContent } from '../domain/CardContent';
 
 const Title = styled.div`
   font-size: 20px;
   font-weight: bold;
 `;
 
+const CardContainer = styled.div`
+  background-color: #fff;
+  border: 1px solid #ddd;
+  border-radius: 20px;
+  box-shadow: 2px 2px 2px 2px rgba(0, 0, 0, 0.1);
+  height: 200px;
+  margin-top: 10px;
+  padding: 8px;
+  width: 288px;
+`;
+
 type Props = {
-  title: string;
-  children: React.ReactNode;
+  cardContent: CardContent;
+  index: number;
 };
 
-const Card: React.FC<Props> = ({ title, children }) => (
+const Card: React.FC<Props> = ({ cardContent, index }) => (
   <div>
-    <Title>{title}</Title>
-    <div>{children}</div>
+    <Draggable draggableId={cardContent.id} index={index}>
+      {draggableProvided => (
+        <CardContainer
+          ref={draggableProvided.innerRef}
+          {...draggableProvided.draggableProps}
+          {...draggableProvided.dragHandleProps}
+        >
+          <Title>{cardContent.title}</Title>
+          <div>{cardContent.body}</div>
+        </CardContainer>
+      )}
+    </Draggable>
   </div>
 );
 
--- src/Kanban/index.tsx
+++ src/Kanban/index.tsx
@@ -1,36 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Droppable, Draggable } from 'react-beautiful-dnd';
+import { Droppable } from 'react-beautiful-dnd';
 import Card from './Card';
+import { CardContent } from '../domain/CardContent';
 
 const KanbanContainer = styled.div`
   padding: 20px;
 `;
 
-type CardContent = {
-  id: string;
-  title: string;
-  body: string;
-};
-
 type Props = {
   cardContents: CardContent[];
 };
 
-const CardContainer = styled.div`
-  background-color: #fff;
-  border: 1px solid #ddd;
-  border-radius: 20px;
-  box-shadow: 2px 2px 2px 2px rgba(0, 0, 0, 0.1);
-  height: 200px;
-  margin-top: 10px;
-  padding: 8px;
-  width: 288px;
-`;
-
 const Kanban: React.FC<Props> = ({ cardContents }) => (
   <KanbanContainer>
-    {/* TODO:列ごとに表示出来るようにする、できればもっとシンプルにする */}
+    {/* TODO:列ごとに表示出来るようにする */}
     <Droppable droppableId="cards">
       {droppableProvided => (
         <div
@@ -39,21 +23,11 @@ const Kanban: React.FC<Props> = ({ cardContents }) => (
         >
           {cardContents.map((cardContent, index) => {
             return (
-              <Draggable
+              <Card
                 key={cardContent.id}
-                draggableId={cardContent.id}
+                cardContent={cardContent}
                 index={index}
-              >
-                {draggableProvided => (
-                  <CardContainer
-                    ref={draggableProvided.innerRef}
-                    {...draggableProvided.draggableProps}
-                    {...draggableProvided.dragHandleProps}
-                  >
-                    <Card title={cardContent.title}>{cardContent.body}</Card>
-                  </CardContainer>
-                )}
-              </Draggable>
+              />
             );
           })}
         </div>
