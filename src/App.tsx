2f42033ac1bd85d2700758e719c1c9a22791706e
wakaholi <tao6128@gmail.com>
2021-04-15 21:46:04 +0900

    カンバンにテストデータを注入

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
 import React from 'react';
+import Kanban from './Kanban';
 
-const App: React.FC = () => <div className="App" />;
+const testCards = [
+  {
+    id: 'nanasaki',
+    title: '七咲逢',
+    body: '水泳部後輩',
+  },
+  {
+    id: 'shinboriRudorufu',
+    title: 'アグネスタキオン',
+    body: 'ハイライトなんていらないんですよやっぱり',
+  },
+  {
+    id: 'cherryBakushinKing',
+    title: 'サクラバクシンオー',
+    body: 'バクシンバクシンバクシンー！！！！！',
+  },
+];
+
+const App: React.FC = () => <Kanban cardContents={testCards} />;
 
 export default App;
