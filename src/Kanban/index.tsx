6d75b29075cd006223a65eddf4b7602971cc818c
wakaholi <tao6128@gmail.com>
2021-04-15 21:45:35 +0900

    カンバンのルートコンポーネント作成

---
+++ src/Kanban/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import styled from 'styled-components';
+import Card from './Card';
+
+const KanbanContainer = styled.div`
+  margin: 20px;
+`;
+
+type CardContent = {
+  id: string;
+  title: string;
+  body: string;
+};
+
+type Props = {
+  cardContents: CardContent[];
+};
+
+const Kanban: React.FC<Props> = ({ cardContents }) => (
+  <KanbanContainer>
+    {cardContents.map(cardContent => {
+      return (
+        <Card key={cardContent.id} title={cardContent.title}>
+          {cardContent.body}
+        </Card>
+      );
+    })}
+  </KanbanContainer>
+);
+
+export default Kanban;
